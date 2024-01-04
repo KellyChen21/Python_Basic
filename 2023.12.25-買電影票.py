e597b2be1673dc65f8f146f42967848fc8fddbf8
KellyChen21 <155596216+KellyChen21@users.noreply.github.com>
2024-01-04 09:44:58 +0800

    Add files via upload

---
+++ 2023.12.25-買電影票.py
@@ -0,0 +1,93 @@
+age = int( input ('請輸入年齡 : '))
+# age = eva1 (input('請輸入年齡'))
+student = input('你是不是學生(Y/N):')
+
+money = 0
+
+if age >= 5 and student =="Y" :
+    money = 100
+elif age >= 5 and student == "N" :
+    if 5 <= age <= 18 :
+        money = 120
+    else :
+        money = 150
+
+print(str(money)+'元')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if age<5 or :
+#     if _________:
+#         print('免費')
+#     else :
+#         print('100元')
+# else :
+#     if______________ :
+#         print('120')
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(age,student)
+
+# if age < 5 :
+#     `print('免費')
+# elif student == "y"
+#     print("100歲")
+# elif 5 <=age and age <= 18
+# print(120元)
+# print
